5b93705108daf0692ea11bfd7897212024df6b29
abdul666333 <114367378+abdul666333@users.noreply.github.com>
2025-02-11 14:03:20 -0500

    Initial Commit

---
+++ TravelAi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelAi
+//
+//  Created by Abdul Alkawasmeh on 2025-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelAi/TravelAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAiApp.swift
+//  TravelAi
+//
+//  Created by Abdul Alkawasmeh on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct TravelAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
